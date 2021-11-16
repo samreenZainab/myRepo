e0e96448b406a97c1c801c17dccf8cfa72d963af
samreen zainab <zainab.samreen256@gamil.com>
2021-11-16 15:44:58 +0500

    nab.samreen256@gamil.com

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require("http");
 const server = http.createServer((req,res) => {
 console.log(req.headers);
 res.end("hello to my response");
-//process.exit();
+process.exit();
 });
 server.listen(3000);
 // const requestListener = function (req,res)
